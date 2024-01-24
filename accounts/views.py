c712a896c04c011c8e787a1f5636977513f7b2cd
Alex <sacha2005d@gmail.com>
2024-01-24 19:35:42 +0900

    create html not full

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import User
+from .form import UserRegistrationForm
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def support(request):
+    return render(request, 'support.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def register(request): 
+    form = UserRegistrationForm() # Определяется пустая форма для регистрации пользователя
+ 
+    if request.method == "POST": # Если пользователь ввёл данные в html форму и отправил их на сервер
+        form = UserRegistrationForm(data=request.POST) # передаём данные в форму
+        if form.is_valid(): # форма проверяет данные
+            user = User.objects.create( # создаём пользователя
+                username = form.cleaned_data['username'], # передаём пользователю username
+                email = form.cleaned_data['email'], # передаём пользователю email
+            )
+            user.set_password(raw_password=form.cleaned_data['password']) # хешируем пароль пользователя
+            user.save() # сохранение пользователя
+            return redirect("accounts:login") # перенаправление пользователя на страницу авторизации
+    
+
+    context = { # определение контекса
+        "form": form
+    }
+    # отправка контекстных данных на html шаблон
+    return render(request=request, template_name="register.html", context=context)
+
+
+
+def test(request):
+    return render(request, 'main_home.html')
