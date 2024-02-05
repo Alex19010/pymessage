e1bc4f4c6ac33e3fa7d8f0b9931e7fa1d7b8c070
Alex <sacha2005d@gmail.com>
2024-02-05 17:24:01 +0900

    create priv chat

---
+++ group_chat/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+class GroupChat(models.Model):
+    members = models.ManyToManyField(User, related_name='group_chats')
+    name = models.CharField(verbose_name='Name' ,max_length=150)
+
+class Meta:
+    verbose_name = 'Group chat'
+    verbose_name_plural = 'Group chats'
+
+
+class GroupMessage(models.Model):
+    chat = models.ForeignKey(GroupChat, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(verbose_name='Message')
+    timestamp = models.DateTimeField(verbose_name='Time' ,auto_now_add=True)
+
+class Meta:
+    verbose_name = 'Group message'
+    verbose_name_plural = 'Group messages'
--- main/settings.py
+++ main/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
+    'private_chat',
+    'group_chat',
 ]
 
 MIDDLEWARE = [
@@ -127,9 +129,15 @@ MEDIA_ROOT = 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 AUTH_USER_MODEL = 'accounts.User'
 
+
 #logout
 LOGOUT_REDIRECT_URL = 'home'
+
+
+AUTHENTICATION_BACKENDS = ['accounts.backends.EmailBackend']
