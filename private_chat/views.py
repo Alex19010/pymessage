56f54a5832ae8952edad36e7b77eef9b469eeb2b
BIG-HAB <Djzeus577@gmail.com>
2024-02-07 17:57:37 +0300

    some up

--- private_chat/views.py
+++ private_chat/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required()
 def private_chat(request):
     return render(request, 'private_chat/chat_private_chat.html')
+
+
+# @login_required
+# def is_log(request):
+#     pass
