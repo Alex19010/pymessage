9f97102166f43818ec3cbf5a08783a5a754b678c
Alex <sacha2005d@gmail.com>
2024-02-09 18:08:39 +0900

    some upd

--- private_chat/views.py
+++ private_chat/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import PrivateChat, PrivateMessage
+
 
 @login_required()
 def private_chat(request):
     return render(request, 'private_chat/chat_private_chat.html')
 
 
-# @login_required
-# def is_log(request):
-#     pass
+# def chat_view(request):
+#     Chat = PrivateChat
+#     Messages = PrivateMessage
+#     user_chats = Chat.objects.filter(members=request.user)  # Получаем все чаты пользователя
+#     messages = []
+#     for chat in user_chats:
+#         chat_messages = chat.Messages.all()  # Получаем все сообщения для каждого чата
+#         messages.extend(chat_messages)
+#     messages = sorted(messages, key=lambda x: x.timestamp)  # Сортируем сообщения по времени
+#     return render(request, 'chat.html', {'messages': messages})
+
+
+def chat_view(request):
+    private_chats = PrivateChat.objects.filter(members=request.user)
+    messages = PrivateMessage.objects.filter(chat__in=private_chats).order_by('timestamp')
+    return render(request, 'chat.html', {'messages': messages})
