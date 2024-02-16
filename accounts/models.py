9874259c259be0ae67a53c60ae112cb8b7d82d92
BIG-HAB <Djzeus577@gmail.com>
2024-02-16 18:05:43 +0300

    'some'

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ from .utils import set_random_username
 class User(AbstractUser):
     username = models.CharField(verbose_name = "Username", max_length = 100, unique = True)
     email = models.EmailField(verbose_name = "Email", unique = True)
-    avatar = models.ImageField(verbose_name = "Avatar", upload_to = "image/", default = "media/defaults/avatar.jpg", null = True, blank = True)
+    avatar = models.ImageField(verbose_name = "Avatar", upload_to = "image/", default = "defaults/avatar.jpg", null = True, blank = True)
     first_name = models.CharField(verbose_name = "First name", max_length = 100)
     last_name = models.CharField(verbose_name = "Last name", max_length = 100, null = True, blank = True)
     date_of_birth = models.DateField(verbose_name = "Date of birth", null = True, blank = True)
@@ -19,8 +19,12 @@ class User(AbstractUser):
         verbose_name_plural = "Users"
 
     def __str__(self):
-        return f"{self.username}"
+        return f"{self.first_name} {self.last_name}"
 
     def save(self, *args, **kwargs) -> None: #Iterable[str] | None = ...:
         self.username = set_random_username()
         return super().save(*args, **kwargs)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}".title()
