6f96833f979671eaa7138663b874be78bce059fe
BIG-HAB <Djzeus577@gmail.com>
2024-03-01 17:59:03 +0300

    'some'

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('posts/like/<int:post_id>/', views.add_like, name='like_post'),
-    path('posts/create_new_post/', views.create_post, name='create_new_post'),
+    path('', views.friends_posts, name='home'),
+    path('my_posts', views.my_posts, name='my_posts'),
+    path('like/<int:post_id>/', views.add_remove_like, name='like_post'),
+    path('create_new_post/', views.create_post, name='create_new_post'),
+    path('comments/<int:post_id>/', views.comments_view, name='comments'),
+    path('comments/create/<int:post_id>/', views.create_comment_view, name='create_comment'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from .models import Post, Comment
 from friends.models import Friend
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 
 @login_required()
-def home(request):
+def friends_posts(request):
     user = request.user
     friends = user.user.values_list('friend__id')
-    posts = Post.objects.filter(owner__id__in = friends)
+    posts = Post.objects.filter(owner__id__in = friends)   
+    search = request.GET.get("search")    
+    if search is not None:
+        posts = posts.filter(name__icontains=search)
+ 
+    paginator = Paginator(posts, 1)
+    posts = paginator.get_page(request.GET.get("page"))
+
     context = {
         "posts": posts,
     }
@@ -18,26 +26,67 @@ def home(request):
 
 
 @login_required()
-def add_like(request, post_id):
+def my_posts(request):
+    user = request.user
+    posts = Post.objects.filter(owner = user)
+    search = request.GET.get("search")    
+    if search is not None:
+        posts = posts.filter(name__icontains=search)
+ 
+    paginator = Paginator(posts, 1)
+    posts = paginator.get_page(request.GET.get("page"))
+    context = {
+        "posts": posts,
+    }
+    return render(request, "posts/my_posts.html", context)
+
+
+@login_required()
+def add_remove_like(request, post_id):
     user = request.user
     post = get_object_or_404(Post, id=post_id)
-    if user in post.owner.favorites.all():
-        post.owner.favorites.remove(post)
+    if post in user.favorites.all():
+        user.favorites.remove(post)
     else:
-        post.owner.favorites.add(post)
+        user.favorites.add(post)
     return redirect(request.META.get('HTTP_REFERER', '/'))
 
 
-@login_required
+@login_required()
 def create_post(request):
     form = PostForm()
     if request.method == 'POST':
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
-            group = form.save()
-            group.members.add(request.user)
+            post = form.save(commit=False)
+            post.owner = request.user
+            post.save()
             return redirect('home')
     context = {
         "form": form,
     }
     return render(request, 'posts/create_new_post.html', context)
+
+
+@login_required()
+def comments_view(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    form = CommentForm()
+    context = {
+        'post': post,
+        'form': form
+    }
+    return render(request, 'posts/comments.html', context)
+
+
+@login_required
+def create_comment_view(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.owner = request.user
+            comment.post = post
+            comment.save()
+    return redirect('comments', post_id=post.id)
